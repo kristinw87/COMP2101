c4ca8179ad4dbd6946d371830370bffbe2bb0977
Kristin Wagar <krisb13@gmail.com>
2022-09-21 13:27:35 -0400

    this is lab 1

---
+++ bash/sysinfo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# my first real script for lab 1
+
+# this shows the fully qualified domain name
+echo "FDQN:"
+hostname 
+
+# this is the OS and version
+echo "Host Info:"
+hostnamectl
+
+# any IP addresses that are not on the 127 network
+echo "IP Addresses:"
+hostname -I
+
+# the amount of space available in only the root filesystem
+echo "Root Filesystem Status:"
+df -h /dev/sda3
+
+exit
